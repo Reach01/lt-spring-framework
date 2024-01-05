f115cdfd750856f27e1860a0490b1d7d9bfe82f5
lutong <534466056@qq.com>
2024-01-05 20:04:01 +0800

    feat: 初始框架

--- src/main/java/com/lutong/spring/ApplicationContext.java
+++ src/main/java/com/lutong/spring/ApplicationContext.java
@@ -1,5 +1,7 @@
 package com.lutong.spring;
 
+import java.lang.annotation.Annotation;
+
 /**
  * @Description
  * @Date 2024/1/5 19:37
@@ -13,6 +15,11 @@ public class ApplicationContext {
     public ApplicationContext(Class appConfig) {
         this.appConfig = appConfig;
         // 1. 扫描
+        if (appConfig.isAnnotationPresent(ComponentScan.class)) {
+            ComponentScan componentAnnotation = (ComponentScan) appConfig.getAnnotation(ComponentScan.class);
+            String path = componentAnnotation.value();
+            System.out.println(path);
+        }
     }
 
     public Object getBean(String beanName){
